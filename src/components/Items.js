ac227566f463c9b625719d90e3253d5bf2f13577
ericagong <48196721+ericagong@users.noreply.github.com>
2023-01-09 18:17:17 +0900

    feat: Item 삭제 기능 추가

--- src/components/Items.js
+++ src/components/Items.js
@@ -8,15 +8,32 @@ export default class Items extends CustomComponent {
     const { items } = this.state;
     return `
 					<ul>
-						${items.map((item) => `<li>${item}</li>`).join("")}
+						${items
+              .map(
+                (item, key) => `
+							<li key='${key}'>
+								${item}
+								<button class='delete' id=${key}>삭제</button>
+							</li>
+						`
+              )
+              .join("")}
 					</ul>
 					<button id='append'>Item 추가</button>
     		`;
   }
   setEventListener() {
-    this.target.querySelector("button").addEventListener("click", () => {
+    this.target.querySelector("#append").addEventListener("click", () => {
       const { items } = this.state;
       this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+
+    this.target.querySelectorAll(".delete").forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        const { items } = this.state;
+        items.splice(event.target.id, 1);
+        this.setState({ items });
+      });
     });
   }
 }
